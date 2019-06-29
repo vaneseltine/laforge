70b35fa7aadc041bc549f4c4b6182553b18f21ec
matvan <matvan@umich.edu>
2019-06-28 21:00:59 -0400

    [docs] Update Sphinx flow; add doc8 to nox

--- noxfile.py
+++ noxfile.py
@@ -1,9 +1,18 @@
+from pathlib import Path
+from shutil import rmtree
+
 import nox
 
 # -r to ovveride and reuse them instead
 nox.options.reuse_existing_virtualenvs = False
 
 SUPPORTED_PYTHONS = ("python3.6", "python3.7")
+
+
+def clean_dir(s):
+    folder = Path(s)
+    if folder.exists():
+        rmtree(folder, ignore_errors=True)
 
 
 @nox.session(python=SUPPORTED_PYTHONS)
@@ -14,16 +23,14 @@ def pytest(session):
     See https://www.kidstrythisathome.com/2017/02/tox-pyodbc-and-appveyor.html
 
     """
-
     session.install("-r", "requirements.txt")
     session.install("-e", ".[all]")
+    clean_dir("./build/coverage")
     session.run(
         "pytest",
         "--cov",
-        "--cov-config setup.cfg",
-        "--cov-report xml:build/coverage/coverage.xml",
-        "--cov-report html:build/coverage/html",
-        "--cov-report annotate:build/coverage/annotate",
+        "--cov-config=setup.cfg",
+        "--cov-report=html:build/coverage",
         "-k-slow",
     )
 
@@ -44,28 +51,36 @@ def slow(session):
     session.run("pytest", "-k", "slow")
 
 
-@nox.session(python="python3.7")
+@nox.session(reuse_venv=True)
+def doc8(session):
+    session.install("-U", "doc8", "Pygments")
+    session.run("doc8", "./docs")
+
+
+@nox.session()
 def sphinx(session):
+    # Treat warnings as errors.
+    session.env["SPHINXOPTS"] = "-W"
     session.install("-e", ".")
     session.install("-r", "./docs/requirements.txt")
-    session.run("rm", "-rf", "./build/sphinx", external=True)
+    clean_dir("./build/sphinx")
     session.run("python", "-m", "sphinx", "-b", "coverage", "./docs", "./docs/_static")
     session.run("python", "setup.py", "build_sphinx", "-b", "html", "-W")
 
 
-@nox.session()
+@nox.session(reuse_venv=True)
 def flake8(session):
-    session.install("flake8")
+    session.install("-U", "flake8")
     session.run("python", "-m", "flake8", "./laforge", "--show-source")
 
 
-@nox.session()
+@nox.session(reuse_venv=True)
 def pylint(session):
-    session.install("pylint")
+    session.install("-U", "pylint")
     session.run("pylint", "./laforge", "-d", "import-error")
 
 
-@nox.session()
+@nox.session(reuse_venv=True)
 def black(session):
-    session.install("black>=19.3b0")
+    session.install("-U", "black")
     session.run("python", "-m", "black", "--target-version", "py36", ".")
